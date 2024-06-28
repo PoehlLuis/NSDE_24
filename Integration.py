09b5ebb16db6c1712334bb510a124b224fd263a8
Max Tschurtschenthaler <max.tschurtschenthaler@student.tugraz.at>
2024-06-28 18:00:31 +0200

    test

--- Integration.py
+++ Integration.py
@@ -41,20 +41,21 @@ def stiffnessMatrix(order,coords,mat_tensor):
      Ke: stiffness matrix of each element
      """
     points, weight = np.polynomial.legendre.leggauss(order)
-    Ke = np.zeros((4,4))
+    Ke = np.zeros((4, 4))
     for i in range(order):
         for j in range(order):
             for j in range(order):
                 xi = points[i]
                 eta = points[j]
+
                 J = Jacobian(xi, eta, coords)
                 detJ = np.linalg.det(J)
                 invJ = np.linalg.inv(J)
 
+                dN_dxi, dN_deta = dNi_(xi, eta)
 
                 global_Na = np.zeros(4)
                 global_Nb = np.zeros(4)
-                dN_dxi, dN_deta = dNi_(xi, eta)
                 for k in range(4):
                     global_Na[k] = invJ[0, 0] * dN_dxi[k] + invJ[0, 1] * dN_deta[k]
                     global_Nb[k] = invJ[1, 0] * dN_dxi[k] + invJ[1, 1] * dN_deta[k]
